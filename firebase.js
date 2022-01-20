3181fb9f715a87593d9bfe139b027d0e12a9ec82
asulon1 <adrien.sulon@epitech.eu>
2022-01-20 13:59:27 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQfsze2W7BsmkxL-jQfDIhenRBPSDdpT0",
-  authDomain: "rotten-tomatoes-68e0b.firebaseapp.com",
-  projectId: "rotten-tomatoes-68e0b",
-  storageBucket: "rotten-tomatoes-68e0b.appspot.com",
-  messagingSenderId: "458068527639",
-  appId: "1:458068527639:web:7039c86f0eafde1ace6a52",
+//  Your Firebase config here
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
